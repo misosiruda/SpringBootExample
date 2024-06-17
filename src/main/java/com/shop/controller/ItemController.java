d3fce0e026895fae1e0a72c658c54172109ee21d
misosiruda <81164760+misosiruda@users.noreply.github.com>
2024-06-17 09:59:07 +0900

    아이템 등록 구현(로직은 없음)

--- src/main/java/com/shop/controller/ItemController.java
+++ src/main/java/com/shop/controller/ItemController.java
@@ -1,13 +1,21 @@
 
 package com.shop.controller;
 
+import com.shop.dto.ItemFormDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ItemController {
+    private static final Logger log = LoggerFactory.getLogger(ItemController.class);
+
     @GetMapping(value = "/admin/item/new")
-    public String itemForm(){
+    public String itemForm(Model model){
+        log.info("--------------------itemForm--------------------");
+        model.addAttribute("itemFormDto", new ItemFormDto());
         return "item/itemForm";
     }
 }
---
+++ src/main/java/com/shop/dto/ItemImgDto.java
@@ -0,0 +1,31 @@
+
+package com.shop.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.modelmapper.ModelMapper;
+
+@Getter
+@Setter
+public class ItemImgDto {
+    private Long id; // 이미지 ID
+
+    private String imgName; // 이미지 이름
+
+    private String oriImgName;  // 원본 이미지 이름
+
+    private String imgUrl;  // 이미지 URL
+
+    private String repImgYn;  // 대표 이미지 여부
+
+    //멤버변수로 ModelMapper객체를 추가, ModelMapper 인스턴스 생성
+    private static ModelMapper modelMapper = new ModelMapper();
+
+    // static 메서드로 주어진 ItemImgDto 객체를 복사하여 새로운 ItemImgDto 객체를 반환
+    // ItemImg 엔티티 객체를 파라미터로 받아서 ItemImgDto로 반환한다.
+    // static 메서드로 선언해 ItemImgDto 객체를 생성하지 않아도 호출할 수 있다.
+    public static ItemImgDto of(ItemImgDto itemImg) {
+        // ModelMapper를 사용하여 객체 매핑
+        return modelMapper.map(itemImg,ItemImgDto.class);
+    }
+}
